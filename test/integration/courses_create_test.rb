be488cc858a867a956d3f39eb80023ddb755af2d
Lena Zheng <lzhengem@gmail.com>
2017-03-05 08:42:58 +0000

    added test forsaving course with same crn

--- test/integration/courses_create_test.rb
+++ test/integration/courses_create_test.rb
@@ -4,7 +4,20 @@ class CoursesCreateTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "creating a valid course" do
+  def setup
+    @course = courses(:programming)
+  end
+  
+  test "should not save an invalid course" do
+    get new_course_path
+  
+    #there is a course with crn 111 in course.yml already
+    assert_no_difference "Course.count" do
+      post courses_path, course: {crn: '111', title: 'something', section:'1'}
+    
+    end
+    assert_template 'new'
+    assert_select "div.alert", "The form contains 1 error:"
   
   end
 end
