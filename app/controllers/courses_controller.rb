2bbefeb28f921941cf1f1d6af6d4eb87531fa54a
Lena Zheng <lzhengem@gmail.com>
2017-03-05 06:38:22 +0000

    added create course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,21 @@
 class CoursesController < ApplicationController
     def index
+        @courses = Course.all
     end
     
     def new
+        @course = Course.new
     end
     def create
+        @course = Course.new(course_params)
+        if @course.save
+            flash[:success] = "Course added!"
+            redirect_to courses_path
+        end
     end
+    
+    private
+        def course_params
+            params.require(:course).permit(:title, :section)
+        end
 end
